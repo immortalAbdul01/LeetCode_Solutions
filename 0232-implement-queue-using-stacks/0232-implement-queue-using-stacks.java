9f890499cbf59f68d85b0519139c0c1cbc7e68ed
Abdul Khan <105938766+immortalAbdul01@users.noreply.github.com>
2025-05-28 15:52:40 +0530

    Time: 0 ms (100%), Space: 40.6 MB (99.94%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 class MyQueue {
 
     private Stack<Integer> stack1;
@@ -6,35 +8,33 @@ class MyQueue {
     public MyQueue() {
         stack1 = new Stack<>();
         stack2 = new Stack<>();
-
     }
 
+    // Costly push
     public void push(int x) {
+        // Move everything from stack1 to stack2
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+
+        // Push x into empty stack1
         stack1.push(x);
 
+        // Move everything back to stack1
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
     }
 
     public int pop() {
-        MoveToStack2();
-        return stack2.pop();
-
+        return stack1.pop(); // simple pop
     }
 
     public int peek() {
-        MoveToStack2();
-        return stack2.peek();
-
+        return stack1.peek(); // simple peek
     }
 
     public boolean empty() {
-        return stack1.isEmpty() && stack2.isEmpty();
-
-    }
-    public void MoveToStack2(){
-        if(stack2.isEmpty()){
-            while(!stack1.isEmpty()){
-                stack2.push(stack1.pop());
-            }
-        }
+        return stack1.isEmpty();
     }
 }

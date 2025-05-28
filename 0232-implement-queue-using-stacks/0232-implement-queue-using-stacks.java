4f081aa7969cebe565083e4cfdee2158ef2cbf59
Abdul Khan <105938766+immortalAbdul01@users.noreply.github.com>
2025-05-28 15:41:36 +0530

    Time: 0 ms (100%), Space: 41.4 MB (68.19%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,5 +1,3 @@
-
-
 class MyQueue {
 
     private Stack<Integer> stack1;
@@ -8,34 +6,33 @@ class MyQueue {
     public MyQueue() {
         stack1 = new Stack<>();
         stack2 = new Stack<>();
+
     }
 
-    // Push element x to the back of queue
     public void push(int x) {
         stack1.push(x);
+
     }
 
-    // Removes the element from in front of queue and returns that element
     public int pop() {
-        moveStack1ToStack2IfNeeded();
+        MoveToStack2();
         return stack2.pop();
+
     }
 
-    // Get the front element
     public int peek() {
-        moveStack1ToStack2IfNeeded();
+        MoveToStack2();
         return stack2.peek();
+
     }
 
-    // Returns whether the queue is empty
     public boolean empty() {
         return stack1.isEmpty() && stack2.isEmpty();
-    }
 
-    // Move elements only if stack2 is empty
-    private void moveStack1ToStack2IfNeeded() {
-        if (stack2.isEmpty()) {
-            while (!stack1.isEmpty()) {
+    }
+    public void MoveToStack2(){
+        if(stack2.isEmpty()){
+            while(!stack1.isEmpty()){
                 stack2.push(stack1.pop());
             }
         }

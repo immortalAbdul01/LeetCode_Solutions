1c5ee74b2c2c9d6024ae3f0228fd44c1b719441b
Abdul Khan <105938766+immortalAbdul01@users.noreply.github.com>
2025-06-05 13:11:16 +0530

    Time: 20 ms (67.69%), Space: 48 MB (93.54%) - LeetHub

--- 1967-longest-substring-of-all-vowels-in-order/1967-longest-substring-of-all-vowels-in-order.java
+++ 1967-longest-substring-of-all-vowels-in-order/1967-longest-substring-of-all-vowels-in-order.java
@@ -1,26 +1,26 @@
 class Solution {
     public int longestBeautifulSubstring(String word) {
-       int len =1;
-       int ans =0;
-       int count = 1;
-       for(int i =1 ;i<word.length();i++){
-        if(word.charAt(i)>=word.charAt(i-1)){
-            len++;
-        
-        if(word.charAt(i)!=word.charAt(i-1)){
-            count++;
+       int maxLen = 0;
+        int count = 1;
+        int len = 1;
 
-        }}
-        else{
-            len=1;
-            count=1;
+        for (int i = 1; i < word.length(); i++) {
+            char prev = word.charAt(i - 1);
+            char curr = word.charAt(i);
+
+            if (curr >= prev) {
+                len++;
+                if (curr != prev) count++;
+            } else {
+                len = 1;
+                count = 1;
+            }
+
+            if (count == 5) {
+                maxLen = Math.max(maxLen, len);
+            }
         }
-        if(count==5){
 
-        ans=Math.max(ans,len);
-        }
-       }
-       return ans;
-
+        return maxLen;
     }
 }

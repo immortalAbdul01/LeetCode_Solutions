67be15b88b8bb205cd9a7fa79aa7c42afe0ca6f7
Abdul Khan <105938766+immortalAbdul01@users.noreply.github.com>
2025-06-12 22:52:42 +0530

    Time: 12 ms (13.15%), Space: 44.7 MB (47.83%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.java
@@ -1,29 +1,12 @@
 class Solution {
-    public int[] frequencySort(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-		for(int x : nums)
-		    map.put(x, map.getOrDefault(x,0)+1);
-		Map<Integer, ArrayList<Integer>> res = new TreeMap<>();
-		
-		for(int i = 1; i<=nums.length; i++){
-		    ArrayList<Integer> ar = new ArrayList<>();
-		    for(Map.Entry<Integer, Integer> key : map.entrySet()){
-		        if(i==key.getValue())
-		            ar.add(key.getKey());
-		    }
-		    if(!ar.isEmpty()){
-		        Collections.sort(ar, Collections.reverseOrder());
-		        res.put(i, ar);
-		    }
-		}		
-		
-		List<Integer> ls = new ArrayList<>();		
-		for(Map.Entry<Integer, ArrayList<Integer>> key : res.entrySet()){
-		    for(int x : key.getValue()){
-		        for(int i = 1; i<=key.getKey(); i++)
-		            ls.add(x);
-		    }
-		}
-        return ls.stream().mapToInt(i->i).toArray();
-    }
+   public int[] frequencySort(int[] nums) {
+	Map<Integer, Integer> map = new HashMap<>();
+	
+	Arrays.stream(nums).forEach(n -> map.put(n, map.getOrDefault(n, 0) + 1));
+
+	return Arrays.stream(nums).boxed()
+			.sorted((a,b) -> map.get(a) != map.get(b) ? map.get(a) - map.get(b) : b - a)
+			.mapToInt(n -> n)
+			.toArray();
 }
+    }

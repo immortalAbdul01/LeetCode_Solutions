17cacb6cb649610d846e6fe33768eff6ac3ab9c2
Abdul Khan <105938766+immortalAbdul01@users.noreply.github.com>
2025-06-29 00:16:35 +0530

    Time: 1 ms (98.04%), Space: 40.2 MB (99.98%) - LeetHub

---
+++ 1127-last-stone-weight/1127-last-stone-weight.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int s : stones) pq.offer(s);
+
+        while (pq.size() > 1)
+            pq.offer(pq.poll() - pq.poll());
+
+        return pq.peek();
+    }
+}

c528cf00f7f7613259123ca858d9d1ccd75fb35b
Abdul Khan <105938766+immortalAbdul01@users.noreply.github.com>
2025-06-29 18:37:48 +0530

    Time: 3 ms (45.96%), Space: 44.6 MB (95.12%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,47 +1,48 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans = new ArrayList<>();
-        char [][] board = new char[n][n];
+      List<List<String>> ans = new ArrayList<>();
+      char [] [] board  = new char [n][n];
 
-        for(int i = 0 ; i<n;i++){
-            Arrays.fill(board[i],'.');
-        }
+      for(int i = 0 ; i<n;i++){
+        Arrays.fill(board[i],'.');
+      }
+
         nQueens(board,0,n,ans);
-        return ans;
-    }
-    static void nQueens(char [] [] board, int row , int n ,  List<List<String>>ans){
-        if(row==n){
-          List<String> res = new ArrayList<>();
-          for(char []r :board  ){
-            res.add(new String(r));
-          }
-          ans.add(res);
-          return;
+      return ans;
+
+
         }
-        for(int j = 0 ; j<n;j++){
-            if(isSafe(board,row,j,n)){
-                board[row][j] = 'Q';
-                nQueens(board,row+1,n,ans);
-                board[row][j] = '.';
+        static void nQueens(char[][] board, int row,int n, List<List<String>>ans){
+            if(row == n ){
+                List<String> result = new ArrayList<>();
+                for(char[] r : board){
+                    result.add(new String(r));
+                }
+                ans.add(result);
+                return;
+            }
+
+            for(int j = 0 ; j<n;j++){
+                if(isSafe(board,row,j,n)){
+                    board[row][j] = 'Q';
+                    nQueens(board,row+1,n,ans);
+                    board[row][j] = '.';
+                }
             }
         }
-        
-    }
-    static boolean isSafe(char [] [] board, int row,int col, int n){
-            for(int j =  0; j<n;j++){
+        static boolean isSafe(char[][] board, int row , int col , int n ){
+            for(int j = 0 ;j<n;j++){
                 if(board[row][j]=='Q') return false;
             }
             for(int i = 0 ; i<n;i++){
                 if(board[i][col]=='Q') return false;
             }
-            for(int i = row, j = col; i>=0 && j>=0; i--,j--){
+            for(int i = row, j =col; i>=0 && j>=0; i--,j--){
                 if(board[i][j]=='Q') return false;
-
             }
-            for(int i = row ,j=col;i>=0 && j<n ; i--,j++){
-                if(board[i][j]=='Q') return false;
-
-             }
-             return true;
+            for(int i = row , j = col; i>=0 && j<n;i--,j++){
+                 if(board[i][j]=='Q') return false;
+            }
+            return true;
         }
 }
